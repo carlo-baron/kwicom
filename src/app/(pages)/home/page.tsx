7b67bb7c25efe7d8ffc519f36e943be9e02ed03b
carlo-baron <carlobaron430@gmail.com>
2025-09-28 18:19:44 +0800

    feature: skeleton for posts

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -9,70 +9,151 @@ import {
     Avatar,
     IconButton,
     Toolbar,
+    Skeleton,
+    Card,
+    CardHeader,
+    CardContent,
 } from '@mui/material';
-
 import AdbIcon from '@mui/icons-material/Adb';
 import SearchIcon from '@mui/icons-material/Search';
-import HomeIcon from '@mui/icons-material/Home';
+import { 
+    useState,
+} from 'react';
 
 export default function Home(){
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     return(
         <Container
-        className="w-full h-screen"
         >
-                <AppBar
-                >
-                    <Container>
-                        <Toolbar
-                        disableGutters
-                        className='flex justify-between'
-                        >
-                            <Box
-                            >
-                                <IconButton
-                                size='large'
-                                >
-                                    <AdbIcon />
-                                </IconButton>
-                                <IconButton
-                                size='large'
-                                >
-                                    <SearchIcon />
-                                </IconButton>
-                            </Box>
-                            <Box
-                            sx={{
-                                display: {
-                                    xs: 'none',
-                                    sm: 'flex'
-                                }
-                            }}
-                            >
-                                <Typography
-                                variant='h4'
-                                noWrap
-                                component='a'
-                                href='/home'
-                                sx={{
-                                    fontWeight: 600,
-                                    letterSpacing: '.3rem', 
-                                    fontFamily: 'monospace',
-                                }}
-                                >
-                                    TRELLITE
-                                </Typography>
-                            </Box>
-                            <Box>
-                                <IconButton
-                                size='large'
-                                sx={{p: 0}}
-                                >
-                                    <Avatar/>
-                                </IconButton>
-                            </Box>
-                        </Toolbar>
-                    </Container>
-                </AppBar>
+            <Navbar />
+            <Toolbar />
+            <Container
+            maxWidth='sm'
+            className='gap-4 flex flex-col pt-4 pb-4  h-fit'
+            disableGutters
+            >
+                {
+                    isLoading ?
+                        (
+                            <>
+                            <PostSkeleton />
+                            <PostSkeleton />
+                            </>
+                        )
+                        :
+                        (
+                            null
+                        )
+                }
+            </Container>
         </Container>
+    );
+}
+
+function PostSkeleton(){
+    return(
+        <Card
+        >
+            <CardHeader
+                avatar={
+                    <Skeleton
+                    variant='circular'
+                    width={50}
+                    height={50}
+                    />
+                }
+                title={
+                    <Skeleton 
+                    variant='text'
+                    height={10}
+                    width='80%'
+                    style={{ marginBottom: 6 }}
+                    />
+                }
+                subheader={
+                    <Skeleton 
+                    variant='text'
+                    height={10}
+                    width='40%'
+                    />
+                }
+            />
+            <Skeleton 
+            variant='rectangular'
+            height={400}
+            />
+            <CardContent>
+                <Skeleton 
+                variant='text'
+                height={10}
+                style={{
+                    marginBottom: 6
+                }}
+                width='100%'
+                />
+                <Skeleton 
+                variant='text'
+                height={10}
+                width='70%'
+                />
+            </CardContent>
+        </Card>
+    );
+}
+
+function Navbar(){
+    return(
+        <AppBar>
+            <Container>
+                <Toolbar
+                disableGutters
+                className='flex justify-between'
+                >
+                    <Box
+                    >
+                        <IconButton
+                        size='large'
+                        >
+                            <AdbIcon />
+                        </IconButton>
+                        <IconButton
+                        size='large'
+                        >
+                            <SearchIcon />
+                        </IconButton>
+                    </Box>
+                    <Box
+                    sx={{
+                        display: {
+                            xs: 'none',
+                            sm: 'flex'
+                        }
+                    }}
+                    >
+                        <Typography
+                        variant='h4'
+                        noWrap
+                        component='a'
+                        href='/home'
+                        sx={{
+                            fontWeight: 600,
+                            letterSpacing: '.3rem', 
+                            fontFamily: 'monospace',
+                        }}
+                        >
+                            TRELLITE
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <IconButton
+                        size='large'
+                        sx={{p: 0}}
+                        >
+                            <Avatar/>
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
